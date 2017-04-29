238e783a2d95685ce03b2a5763cd79757593ff04
german <gbravoescobar@gmail.com>
2017-04-29 17:10:14 +0200

    Fixed docker starter script

--- docker_xfce.sh
+++ docker_xfce.sh
@@ -3,4 +3,4 @@ if $(docker ps |  grep --quiet selenium)  ; then
 	echo "Killing...."
 	docker kill selenium
 fi
-docker run -p 2222:22 -t -d  -v $(pwd):/data -e PS=$1 --rm --name selenium  german/ubuntu-xfce-selenium
+docker run -p 2222:22 -t -d  -v $(pwd):/data -e PS=$1 --rm --name selenium  german/ubuntu-xfce-x2go
