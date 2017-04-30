1bea06d42f51cc6b86589394b8f9a97f35ea9a56
german <gbravoescobar@gmail.com>
2017-04-30 13:01:12 +0200

    Change name in docker starting script

--- docker_xfce.sh
+++ docker_xfce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if $(docker ps |  grep --quiet selenium)  ; then
 	echo "Killing...."
-	docker kill selenium
+	docker kill ubuntu_xfce
 fi
-docker run -p 2222:22 -t -d  -v $(pwd):/data -e PS=$1 --rm --name selenium  german/ubuntu-xfce-x2go
+docker run -p 2222:22 -t -d  -v $(pwd):/data -e PS=$1 --rm --name ubuntu_xfce  german/ubuntu-xfce-x2go
